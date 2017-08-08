a677a112264fbd05f913941469a1a48be3a99fae
ghanimmukhtar <ghanimmukhtar@gmail.com>
2017-08-08 11:52:38 +0200

    first working version by modifying the (acm) and adding <octomap> to the default entry

--- src/test_acm_moveit.cpp
+++ src/test_acm_moveit.cpp
@@ -1,4 +1,6 @@
 #include <ros/ros.h>
+#include <iostream>
+#include <sstream>
 #include <moveit/move_group_interface/move_group.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <moveit/planning_scene_monitor/planning_scene_monitor.h>
@@ -16,13 +18,24 @@ void planning_scene_cb(const moveit_msgs::PlanningSceneConstPtr& psm){
     acm = psm->allowed_collision_matrix;
     std::vector<std::string> acm_entry_names;
     acm.getAllEntryNames(acm_entry_names);
+    std::ostream stream(nullptr);
+    stream.rdbuf(std::cout.rdbuf());
+//    acm.print(stream);
+
+//    collision_detection::AllowedCollision::Type type;
+//    acm.getDefaultEntry("<octomap>", type);
+
+//    ROS_INFO_STREAM("The octomap collision type is: " << type);
+
+//    ROS_WARN("------------------------------------------------------");
+    /*
     for(size_t i = 0; i < acm_entry_names.size(); i++)
         ROS_INFO_STREAM("The Entry number: " << i << " is: " << acm_entry_names[i]);
     ROS_WARN("****************************************************");
 
     for(size_t i = 0; i < psm->world.collision_objects.size(); i++)
         ROS_INFO_STREAM("The Entry number: " << i << " is: " << psm->world.collision_objects[i]);
-    ROS_WARN("-------------------------------------------------------");
+    ROS_WARN("-------------------------------------------------------");*/
 
 }
 
@@ -31,7 +44,9 @@ int main(int argc, char **argv){
     ros::NodeHandle n;
 
     ros::ServiceClient clear_octomap = n.serviceClient<std_srvs::Empty>("/dream_babbling/controller_node/clear_octomap");
-    ros::Subscriber psm_sub = n.subscribe<moveit_msgs::PlanningScene>("/dream_babbling/controller_node/move_group/planning_scene", 1, planning_scene_cb);
+    ros::Subscriber psm_sub = n.subscribe<moveit_msgs::PlanningScene>("move_group/monitored_planning_scene", 1, planning_scene_cb);
+    ros::Publisher psm_pub = n.advertise<moveit_msgs::PlanningScene>("planning_scene", 1);
+    ros::ServiceClient get_ps = n.serviceClient<moveit_msgs::GetPlanningScene>("get_planning_scene");
 
     ros::AsyncSpinner my_spin(2);
     my_spin.start();
@@ -52,43 +67,57 @@ int main(int argc, char **argv){
 
     bool clear_map = false;
     n.getParam("/clear_octomap", clear_map);
-    if(clear_map){
-        ROS_INFO("I am Clearing the Octomap !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        clear_octomap.call(empty_octomap_request, empty_octomap_response);
-    }
+//    if(clear_map){
+//        ROS_INFO("I am Clearing the Octomap !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+//        clear_octomap.call(empty_octomap_request, empty_octomap_response);
+//    }
 
-    const std::string PLANNING_SCENE_SERVICE = "get_planning_scene";
-    planning_scene_monitor::PlanningSceneMonitorPtr psm =
-            boost::make_shared<planning_scene_monitor::PlanningSceneMonitor>("robot_description");
+//    const std::string PLANNING_SCENE_SERVICE = "get_planning_scene";
+//    planning_scene_monitor::PlanningSceneMonitorPtr psm =
+//            boost::make_shared<planning_scene_monitor::PlanningSceneMonitor>("robot_description");
 
-    psm->requestPlanningSceneState(PLANNING_SCENE_SERVICE);
+//    psm->requestPlanningSceneState(PLANNING_SCENE_SERVICE);
 
-    planning_scene_monitor::LockedPlanningSceneRW ps(psm);
+//    planning_scene_monitor::LockedPlanningSceneRW ps(psm);
 
-
-
-    //ps->getCurrentStateNonConst().update();
-
-    //planning_scene::PlanningScenePtr planning_scene_(new planning_scene::PlanningScene());
-    //planning_scene_monitor::PlanningSceneMonitorPtr ps(new planning_scene_monitor::PlanningSceneMonitor());
+//    ps->getCurrentStateNonConst().update();
 
     std::vector<std::string> acm_entry_names;
-    ps->getAllowedCollisionMatrixNonConst().getAllEntryNames(acm_entry_names);
+//    ps->getAllowedCollisionMatrixNonConst().getAllEntryNames(acm_entry_names);
 
-    ps->getAllowedCollisionMatrixNonConst().setEntry("<octomap>", acm_entry_names, true);
+//    ps->getAllowedCollisionMatrixNonConst().setEntry("<octomap>", acm_entry_names, true);
+
+    moveit_msgs::GetPlanningScene::Request ps_req;
+    moveit_msgs::GetPlanningScene::Response ps_res;
+
+    ps_req.components.components = moveit_msgs::PlanningSceneComponents::ALLOWED_COLLISION_MATRIX;
+
+    get_ps.call(ps_req, ps_res);
+
+    ROS_INFO_STREAM("From Service Response, default acm names size is: " << ps_res.scene.allowed_collision_matrix.default_entry_names.size());
+    ROS_INFO_STREAM("From Service Response, acm names size is: " << ps_res.scene.allowed_collision_matrix.entry_names.size());
+
+
+    ps_res.scene.allowed_collision_matrix.default_entry_names.push_back("<octomap>");
+    ps_res.scene.allowed_collision_matrix.default_entry_values.push_back(true);
+
+    moveit_msgs::PlanningScene ps_msg;
+    ps_msg.is_diff = true;
+    ps_msg.allowed_collision_matrix = ps_res.scene.allowed_collision_matrix;
+
 //    acm.setEntry("octomap", acm_entry_names, true);
 
-    ps->getAllowedCollisionMatrixNonConst().getAllEntryNames(acm_entry_names);
-    for(size_t i = 0; i < acm_entry_names.size(); i++)
-        ROS_INFO_STREAM("The Entry number: " << i << " is: " << acm_entry_names[i]);
+//    ps->getAllowedCollisionMatrixNonConst().getAllEntryNames(acm_entry_names);
+//    for(size_t i = 0; i < acm_entry_names.size(); i++)
+//        ROS_INFO_STREAM("The Entry number: " << i << " is: " << acm_entry_names[i]);
 
-    std::vector<std::string> objects_ids = ps->getCollisionWorld()->getWorld()->getObjectIds();
+//    std::vector<std::string> objects_ids = ps->getCollisionWorld()->getWorld()->getObjectIds();
 
-    ROS_WARN("****************************************************");
+//    ROS_WARN("****************************************************");
 
-    for(size_t i = 0; i < objects_ids.size(); i++)
-        ROS_INFO_STREAM("The Entry number: " << i << " is: " << objects_ids[i]);
-    ROS_WARN("-------------------------------------------------------");
+//    for(size_t i = 0; i < objects_ids.size(); i++)
+//        ROS_INFO_STREAM("The Entry number: " << i << " is: " << objects_ids[i]);
+//    ROS_WARN("-------------------------------------------------------");
 
 
 
@@ -106,11 +135,11 @@ int main(int argc, char **argv){
 //        ROS_WARN("-------------------------------------------------------");
     //ROS_ERROR_STREAM("Finished preparing  !!!!!!!!!!!!!!!!");
 
-//    double x, y, z, xmin = 0.3, xmax = 0.8, ymin = -0.2, ymax = 0.70, zmin = 0.2, zmax = 0.5;
+    double x, y, z, xmin = 0.3, xmax = 0.8, ymin = -0.2, ymax = 0.70, zmin = 0.2, zmax = 0.5;
 
-    /*while(ros::ok()){
-        planning_scene->setPlanningSceneDiffMsg(to_maintain_psm);
-        ROS_ERROR_STREAM("The active collision detector name is: " << planning_scene->getActiveCollisionDetectorName());
+    while(ros::ok()){
+        if(clear_)
+        psm_pub.publish(ps_msg);
         MoveGroup::Plan plan;
         x = (xmax - xmin) * ( (double)rand() / (double)RAND_MAX ) + xmin;
         y = (ymax - ymin) * ( (double)rand() / (double)RAND_MAX ) + ymin;
@@ -121,39 +150,44 @@ int main(int argc, char **argv){
         ROS_ERROR_STREAM("The target is set, now we plan to go to X = " << x << ", Y = " << y << " and Z = " << z);
         if(group.plan(plan)){
             ROS_ERROR_STREAM("I Found a plan I will do nothing !!!!");
-            //group.execute(plan);
+            group.execute(plan);
         }
         else{
             ROS_WARN("No plan found, or Something went wrong to prevent the successful feedback of the planning");
-            robot_state::RobotState& current_robot_state = planning_scene->getCurrentStateNonConst();
-            collision_detection::CollisionRequest collision_request;
-            collision_detection::CollisionResult collision_result;
-            collision_request.contacts = true;
-            collision_request.max_contacts = 1000;
-            ROS_ERROR_STREAM("This failed trajectory has: " << plan.trajectory_.joint_trajectory.points.size() << " points");
-            for(size_t i = 0; i < plan.trajectory_.joint_trajectory.points.size(); i++){
-                collision_result.clear();
-                moveit::core::jointTrajPointToRobotState(plan.trajectory_.joint_trajectory, i, current_robot_state);
-                planning_scene->checkCollision(collision_request, collision_result, current_robot_state, acm);
-                ROS_INFO_STREAM("Test: Current state is "
-                                << (collision_result.collision ? "in" : "not in")
-                                << " self collision");
-                collision_detection::CollisionResult::ContactMap::const_iterator it;
-                for(it = collision_result.contacts.begin();
-                    it != collision_result.contacts.end();
-                    ++it)
-                {
-                    ROS_INFO("Contact between: %s and %s",
-                             it->first.first.c_str(),
-                             it->first.second.c_str());
-                }
-                //current_robot_state.
-            }
+            ps_req.components.components = moveit_msgs::PlanningSceneComponents::ROBOT_STATE;
+//            get_ps.call(ps_req, ps_res);
+
+
+//            robot_state::RobotState current_robot_state;
+//            moveit::core::robotStateMsgToRobotState(ps_res.scene.robot_state, current_robot_state);
+//            collision_detection::CollisionRequest collision_request;
+//            collision_detection::CollisionResult collision_result;
+//            collision_request.contacts = true;
+//            collision_request.max_contacts = 1000;
+//            ROS_ERROR_STREAM("This failed trajectory has: " << plan.trajectory_.joint_trajectory.points.size() << " points");
+//            for(size_t i = 0; i < plan.trajectory_.joint_trajectory.points.size(); i++){
+//                collision_result.clear();
+//                moveit::core::jointTrajPointToRobotState(plan.trajectory_.joint_trajectory, i, current_robot_state);
+//                ->checkCollision(collision_request, collision_result, current_robot_state, acm);
+//                ROS_INFO_STREAM("Test: Current state is "
+//                                << (collision_result.collision ? "in" : "not in")
+//                                << " self collision");
+//                collision_detection::CollisionResult::ContactMap::const_iterator it;
+//                for(it = collision_result.contacts.begin();
+//                    it != collision_result.contacts.end();
+//                    ++it)
+//                {
+//                    ROS_INFO("Contact between: %s and %s",
+//                             it->first.first.c_str(),
+//                             it->first.second.c_str());
+//                }
+//                //current_robot_state.
+//            }
         }
         ROS_INFO("PRESS ENTER For Another try ....");
         std::cin.ignore();
         //ros::spinOnce();
-    }*/
+    }
 
 
     return 0;
